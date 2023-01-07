02baaa101804c9fb99b48f05ab8fe42b74ed491c
Hoàng Như Nghĩa BAT Tech <nghiahn@batgroup.vn>
2023-01-08 01:16:08 +0700

    update score

--- scripts/index.js
+++ scripts/index.js
@@ -195,6 +195,9 @@ class Board {
     constructor(ctx) {
         this.ctx = ctx;
         this.grid = this.generateWhiteBoard();
+        this.score = 0;
+        this.gameOver = false;
+        this.isPlaying = false;
     }
 
     generateWhiteBoard() {
@@ -224,25 +227,29 @@ class Board {
         const newScore = ROWS - lastesGrid.length;
         const newRows = Array.from({ length: newScore }, () => Array(COLS).fill(WHITE_COLOR_ID));
 
-        board.grid = [...newRows, ...lastesGrid];
+        if (newScore) {
+            board.grid = [...newRows, ...lastesGrid];
+            this.handleScore(newScore * 10);
+        }
     }
 
-    // handleCompleteRows() {
-    //     const latestGrid = board.grid.filter((row) => { // row => []
-    //         return row.some(col => col === WHITE_COLOR_ID);
-    //     });
+    handleScore(newScore) {
+        this.score += newScore;
+        document.getElementById('score').innerHTML = this.score;
+    }
 
-    //     const newScore = ROWS - latestGrid.length; // => newScore = tong cong hang da hoan thanh
-    //     const newRows = Array.from({ length: newScore }, () => Array(COLS).fill(WHITE_COLOR_ID));
+    handleGameOver() {
+        this.gameOver = true;
+        this.isPlaying = false;
+        alert('Game over!!!');
+    }
 
-    //     if (newScore) {
-    //         board.grid = [...newRows, ...latestGrid];
-    //         this.handleScore(newScore * 10);
-
-    //         this.clearAudio.play();
-    //         console.log({ latestGrid });
-    //     }
-    // }
+    reset() {
+        this.score = 0;
+        this.grid = this.generateWhiteBoard();
+        this.gameOver = false;
+        this.drawBoard();
+    }
 }
 
 class Brick {
@@ -325,20 +332,12 @@ class Brick {
     }
 
 
-    // checkCollision(nextRow, nextCol, nextLayout) {
-    //     for (let row = 0; row < nextLayout.length; row++) {
-    //         for (let col = 0; col < nextLayout[0].length; col++) {
-    //             if (nextLayout[row][col] !== WHITE_COLOR_ID) {
-    //                 if (col + nextCol < 0 || col + nextCol >= COLS || row + nextRow >= ROWS
-    //                 )
-    //                     return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
-
     handleLanded() {
+        if (this.rowPos <= 0) {
+            board.handleGameOver();
+            return;
+        }
+
         for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
             for (let col = 0; col < this.layout[this.activeIndex][0].length; col++) {
                 if (this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
@@ -350,37 +349,49 @@ class Brick {
         board.drawBoard();
     }
 
+
 }
-
-board = new Board(ctx);
-board.drawBoard();
-
 
 function generateNewBrick() {
     brick = new Brick(Math.floor(Math.random() * 10) % BRICK_LAYOUT.length);
 }
+board = new Board(ctx);
+board.drawBoard();
 
-generateNewBrick();
+document.getElementById("play").addEventListener('click', () => {
+    board.reset();
+    board.isPlaying = true;
+    generateNewBrick();
+    const refresh = setInterval(() => {
+        if (!board.gameOver) {
+            brick.moveDown();
+        } else {
+            clearInterval(refresh);
+        }
+    }, 1000);
+})
 
-setInterval(() => {
-    brick.moveDown();
-}, 1000);
+
+
 
 document.addEventListener('keydown', (e) => {
-    switch (e.code) {
-        case KEY_CODES.LEFT:
-            brick.moveLeft();
-            break;
-        case KEY_CODES.RIGHT:
-            brick.moveRight();
-            break;
-        case KEY_CODES.DOWN:
-            brick.moveDown();
-            break;
-        case KEY_CODES.UP:
-            brick.rotate();
-            break;
-        default:
-            break;
+
+    if (!board.gameOver && board.isPlaying) {
+        switch (e.code) {
+            case KEY_CODES.LEFT:
+                brick.moveLeft();
+                break;
+            case KEY_CODES.RIGHT:
+                brick.moveRight();
+                break;
+            case KEY_CODES.DOWN:
+                brick.moveDown();
+                break;
+            case KEY_CODES.UP:
+                brick.rotate();
+                break;
+            default:
+                break;
+        }
     }
 })
